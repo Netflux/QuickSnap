591b9830145d165387c3ccc9434960f46a332db2
Victor <victorlyc97@gmail.com>
2016-09-08 17:44:17 +0800

    Add interesting font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,7 +11,8 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
-        }
+			SwinGame.LoadFontNamed("GameFont","Chunkfive.otf",24);
+		}
 
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
@@ -19,8 +20,11 @@ namespace CardGames
 		/// <param name="myGame">The game object to update in response to events.</param>
 		private static void HandleUserInput(Snap myGame)
 		{
+			
 			//Fetch the next batch of UI interaction
 			SwinGame.ProcessEvents();
+	
+
 
 			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
 			{
@@ -37,13 +41,14 @@ namespace CardGames
 			SwinGame.ClearScreen(Color.White);
 			SwinGame.DrawBitmap ("cardsBoard.png", 0, 0);
 
+
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				
+				SwinGame.DrawText ("" + myGame.Score (0), Color.White, "GameFont", 77, 55);
+				SwinGame.DrawText ("" + myGame.Score (1), Color.White, "GameFont", 768, 55);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
@@ -53,7 +58,7 @@ namespace CardGames
 
 			// Draw the back of the cards... to represent the deck
 			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 155, 153);
-
+		
 			//Draw onto the screen
 			SwinGame.RefreshScreen(60);
 		}
